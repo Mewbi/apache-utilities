c43b07e5db5ffb9fa2afb1f183d7e2ec0424cdcc
Felipe <46628384+Mewbi@users.noreply.github.com>
2019-09-05 09:43:47 -0300

    Update create.sh

--- create.sh
+++ create.sh
@@ -3,7 +3,7 @@
 #Verificando arquivos necessários
 if [ -s "apache-utilities-create.txt" ]; then
 	source apache-utilities-create
-
+	echo -e "\nArquivo "apache-utilities-create" encontrado!\n"
 else
 	cat << END
 
